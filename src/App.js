00a17f221d5232527101cf04c292bb2c444f9b03
MastaM1ND <owenmbarry@gmail.com>
2023-11-01 17:55:22 -0400

    navbar and card addition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
+import Navbar from "./Navbar.js";
+import Card from "./Card.js";
 
 function App() {
   return (
-    <div className="App">
+    <><Navbar /><div className="App">
       <h1>CS 230L</h1>
       <h2>Section - 003</h2>
       <p>WVU ID: 800402387</p>
       <p>Hi I am Owen</p>
-    </div>
+    </div><Card /></>
+
   );
 }
 
