109490952582ff70d6c2cbc50ede91151dce78ac
MastaM1ND <owenmbarry@gmail.com>
2023-11-01 18:10:51 -0400

    Changed navbar and card

--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,36 @@
 import React from "react";
 
-const Card = () => {
+function Card(){
   return (
     
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-          <div class="card-header">Header</div>
-          <div class="card-body">
-                <h5 class="card-title">Primary card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             </div>
-        </div><div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-              <div class="card-header">Header</div>
-              <div class="card-body">
-                  <h5 class="card-title">Secondary card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-          </div><div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-              <div class="card-header">Header</div>
-              <div class="card-body">
-                  <h5 class="card-title">Success card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-          </div>
-    </div>
+    <div className="Card" class="container-fluid mt-3">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card text-white bg-primary mb-3"  >
+                        <div class="card-header">Card 01</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card text-white bg-warning mb-3"  >
+                        <div class="card-header">Card 02</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card text-white bg-success mb-3"  >
+                        <div class="card-header">Card 03</div>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   );
 };
 
